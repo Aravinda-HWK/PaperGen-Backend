8919037b8f91fe176ca099115a54a2b6d1103420
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2024-01-10 13:48:33 +0530

    create simple business logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  test() {
-    return 'test';
+  signup() {
+    return {
+      message: 'I am sign in',
+    };
+  }
+
+  signin() {
+    return {
+      message: 'I am sign up',
+    };
   }
 }
