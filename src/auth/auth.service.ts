635874a37e6f8d36a5489c08af948bc6e03c55f6
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2024-01-16 15:57:28 +0530

    forbidden Exception error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -11,15 +12,26 @@ export class AuthService {
     console.log(dto);
     const hash = await argon.hash(dto.password);
     // Create the user record in the database
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    // Return the user record
-    delete user.hash;
-    return user;
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      // Return the user record
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+
+      throw error;
+    }
   }
 
   signin() {
