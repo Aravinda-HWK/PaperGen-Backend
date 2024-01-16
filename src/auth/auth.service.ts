e60439654c2f66738b58f09af1a01081ad0ae6d5
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2024-01-16 15:51:24 +0530

    Add some migration scripts for the database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return {
-      message: 'I am sign in',
-    };
+  async signup(dto: AuthDto) {
+    // Generate the password hash (salt + hash)
+    console.log(dto);
+    const hash = await argon.hash(dto.password);
+    // Create the user record in the database
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    // Return the user record
+    delete user.hash;
+    return user;
   }
 
   signin() {
