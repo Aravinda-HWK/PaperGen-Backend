3a3ebb64917f3e8a0ca1c36fc9f99125a3ce35d0
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2024-03-20 12:23:05 +0530

    Remove unnecessary models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, BookmarkModule, AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule],
 })
 export class AppModule {}
