a56965b3babceebcc1566050a874aa125a05740a
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2024-03-23 19:27:59 +0530

    Create classroom by teacher

--- src/classroom/classroom.controller.ts
+++ src/classroom/classroom.controller.ts
@@ -1,7 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Put } from '@nestjs/common';
 import { ClassroomService } from './classroom.service';
 
 @Controller('classroom')
 export class ClassroomController {
   constructor(private classroomService: ClassroomService) {}
+
+  @Put()
+  async createClassroom(@Body() data: { name: string; teacherID: any }) {
+    return this.classroomService.createClassroom(data);
+  }
 }
--- src/classroom/classroom.service.ts
+++ src/classroom/classroom.service.ts
@@ -5,4 +5,44 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable()
 export class ClassroomService {
   constructor(private prisma: PrismaService) {}
+
+  async createClassroom(dto: { teacherID: any; name: string }) {
+    try {
+      let teacher: {
+        id: any;
+        createdAt?: Date;
+        updatedAt?: Date;
+        email?: string;
+        password?: string;
+        firstName?: string;
+        lastName?: string;
+        photo?: Buffer;
+        description?: string;
+      };
+      try {
+        teacher = await this.prisma.teacher.findUnique({
+          where: { id: dto.teacherID },
+        });
+      } catch {
+        throw new ForbiddenException(
+          'You are not allowed to create a classroom',
+        );
+      }
+      return await this.prisma.classroom.create({
+        data: {
+          name: dto.name,
+          teacher: {
+            connect: { id: teacher.id },
+          },
+        },
+      });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        throw new ForbiddenException(
+          'You are not allowed to create a classroom',
+        );
+      }
+      throw error;
+    }
+  }
 }
