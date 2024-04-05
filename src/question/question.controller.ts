666be2562a12bf4596b97a415136d680754499fb
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-04-05 20:20:45 +0530

    Create question api

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { QuestionService } from './question.service';
 
 @Controller('question')
 export class QuestionController {
   constructor(private questionService: QuestionService) {}
+
+  @Post('create')
+  async create(@Body() data: any) {
+    return await this.questionService.create(data);
+  }
 }
--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -5,4 +5,23 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable()
 export class QuestionService {
   constructor(private prisma: PrismaService) {}
+
+  // Add a new method to the QuestionService class that creates a new question.
+  async create(data: any) {
+    try {
+      return await this.prisma.question.create({
+        data: {
+          content: data.content,
+          sampleAnswer: data.sampleAnswer,
+          correctAnswer: data.correctAnswer,
+          paper: { connect: { id: data.paperId } },
+        },
+      });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        throw new ForbiddenException(error.message);
+      }
+      throw error;
+    }
+  }
 }
