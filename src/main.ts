afc4b51d29d373f0ec72f11e3ce9c198c27bc856
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-05-27 11:47:20 +0530

    Add cors to access from frontend

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.use(
+    cors({
+      origin: 'http://localhost:3000', // Replace with your frontend URL
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true, // Allow cookies
+    }),
+  );
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
