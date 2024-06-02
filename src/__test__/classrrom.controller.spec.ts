b4e1dce18669f8718662ce7f0690ff49c7e45b1b
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-06-02 17:18:47 +0530

    Testing new organization

--- src/__test__/classrrom.controller.spec.ts
+++ src/__test__/classrrom.controller.spec.ts
@@ -1,5 +1,4 @@
 describe('ClassroomController', () => {
   it('should be defined', () => {});
   it('should create a new classroom', async () => {});
-  it('should throw an error if the classroom already exists', async () => {});
 });
