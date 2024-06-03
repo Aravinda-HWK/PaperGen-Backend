84c02c072630d07a3ef581cb92e47c880111f038
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-06-03 17:52:24 +0530

    Add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,11 @@ import { ClassroomModule } from './classroom/classroom.module';
 import { PaperModule } from './paper/paper.module';
 import { QuestionModule } from './question/question.module';
 import { ResultModule } from './result/result.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     PrismaModule,
     StudentModule,
