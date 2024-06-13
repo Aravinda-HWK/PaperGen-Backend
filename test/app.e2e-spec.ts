d5c6c85de55bac92790c7c4fee7f71a752b13a7c
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-06-13 10:10:14 +0530

    create new test database

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,37 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 describe('App e2e', () => {
+  let app: INestApplication;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+
+    app.use(
+      cors({
+        origin: 'http://localhost:3000', // Replace with your frontend URL
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true, // Allow cookies
+      }),
+    );
+
+    await app.init();
   });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
   it.todo('should display welcome message');
 });
